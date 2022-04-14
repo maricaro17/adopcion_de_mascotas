34350c96ca66069a50cec0d5f5481db38944a040
Mariana Camacho <marianacamacho700@gmail.com>
2022-04-13 19:05:37 -0500

    base del proyecto

---
+++ JS/category.js
@@ -0,0 +1,13 @@
+const btnHome = document.getElementById("btn-home")
+const btnPerfil = document.getElementById("btn-perfil")
+const btnFavoritos = document.getElementById("btn-favoritos")
+const btnMensajes = document.getElementById("btn-mensajes")
+const textHome = document.getElementById("home-text")
+const textPerfil = document.getElementById("perfil-text")
+const textFavoritos = document.getElementById("favoritos-text")
+const textMensajes = document.getElementById("mensajes-text")
+const iconHome = document.getElementById("home-icon")
+const iconPerfil = document.getElementById("perfil-icon")
+const iconFavoritos = document.getElementById("favoritas-icon")
+const iconMensajes = document.getElementById("mensajes-icon")
+
---
+++ JS/index.js
@@ -0,0 +1,3 @@
+setTimeout(()=>{
+    window.location.href="pages/main.html"
+}, 3000)
